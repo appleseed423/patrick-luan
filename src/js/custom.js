e6ee7ad86484e8815f8532d4f4f35b4200e4c99c
appleseed423 <superstar.diegoherrera@gmail.com>
2025-02-24 11:43:54 +0900

    first commit

---
+++ src/js/custom.js
@@ -0,0 +1,50 @@
+const navLinks = document.querySelectorAll('.nav-all ul li a');
+const sideNavLinks = document.querySelectorAll('.mobile-menu ul li a');
+const header = document.getElementById('header');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        navLinks.forEach(nav => nav.classList.remove('active'));
+        this.classList.add('active');
+        mobileMenuWrapper.style.visibility = 'hidden';
+    });
+});
+
+sideNavLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        sideNavLinks.forEach(nav => nav.classList.remove('active_nav'));
+        this.classList.add('active_nav');
+        mobileMenuWrapper.style.visibility = 'hidden';
+    });
+});
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 10) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+(function() {
+    emailjs.init("jVejH8qQen5dp6PTm"); // Replace with your actual user ID
+})();
+
+// Handle form submission
+document.getElementById("contact_form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting the traditional way
+    // Prepare the data to send
+    const from_name = document.getElementById("from_name").value;
+    const message = document.getElementById("message").value;
+    const reply_to = document.getElementById("reply_to").value;
+
+    emailjs.send("service_3g9zrqf", "template_xm0ql69", {
+        from_name: from_name,
+        message: message,
+        from_mail: reply_to,
+    }).then(function(response) {
+        alert("Success!");
+    }, function(error) {
+        alert("Failed!");
+    });
+});
